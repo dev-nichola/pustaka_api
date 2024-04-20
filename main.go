5464ed5f7364e719e48a046b3b0ef6aa310a7114
Nichola Saputra <kerjasamakol@gmail.com>
2024-04-20 19:05:31 +0700

    error validation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -12,6 +14,7 @@ func main() {
 	router.GET("/hello", helloHandler)
 	router.GET("/book/:id/title", bookHandler)
 	router.GET("/query", queryHandler)
+	router.POST("/books", postBooksHandler)
 
 	router.Run(":8000")
 }
@@ -47,4 +50,34 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name": "Hello World",
 	})
+}
+
+type BookInput struct {
+	Title      string `json:"title" binding:"required"`
+	Price      int    `json:"price" binding:"required,number"`
+	BookAuthor string `json:"book_author"`
+}
+
+func postBooksHandler(c *gin.Context) {
+	var BookInput BookInput
+
+	err := c.ShouldBindJSON(&BookInput)
+
+	if err != nil {
+
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error On Field %s, condition %s", e.Field(), e.ActualTag())
+			c.JSON(http.StatusBadRequest, errorMessage)
+
+			return
+		}
+
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":       BookInput.Title,
+		"harga":       BookInput.Price,
+		"book_author": BookInput.BookAuthor,
+	})
+
 }
